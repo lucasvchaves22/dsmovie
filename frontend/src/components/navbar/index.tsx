6dfda72b35695cad7fe6d13426ce80d5ec0aa0ac
Lucas Notebook <lucasvchaves22@gmail.com>
2022-01-13 17:05:52 -0300

    Navbar

---
+++ frontend/src/components/navbar/index.tsx
@@ -0,0 +1,24 @@
+import { ReactComponent as GithubIcon } from 'assets/img/github.svg'
+import './styles.css'
+
+function Navbar() {
+
+    return (
+        <header>
+            <nav className="container">
+                <div className="dsmovie-nav-content">
+                    <h1>DSMovie</h1>
+                    <a href="https://github.com/lucasvchaves22">
+                        <div className="dsmovie-contat-container">
+                            <GithubIcon />
+                            <p className="dsmovie-contact-link">/lucasvchaves22</p>
+                        </div>
+                    </a>
+                </div>
+            </nav>
+        </header>
+    );
+
+}
+
+export default Navbar;
